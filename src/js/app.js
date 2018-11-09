f61d9a0a81594c551e3b2439d2fba1614ff80bda
Deiber Chacón <deiberchacon@gmail.com>
2018-11-09 11:29:19 -0600

    Create the Vue instance

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
-(function() {
-    console.log('Hello world');
-})();
+var Vue = require('vue');
+Vue.config.debug = true;
+
+// Vue instance
+new Vue({
+    el: '#container',
+    data: {
+        fullRepoName: '',
+        username: '',
+        repo: ''
+    },
+    methods: {
+        changeRepo: function () {
+            var splitData = this.fullRepoName.split('/');
+            this.username = splitData[0];
+            this.repo = splitData[1];
+
+            console.group("Vue Data");
+            console.log("fullRepoName:", this.fullRepoName);
+            console.log("username:", this.username);
+            console.log("repo:", this.repo);
+            console.groupEnd("Vue Data");
+        }
+    }
+});
